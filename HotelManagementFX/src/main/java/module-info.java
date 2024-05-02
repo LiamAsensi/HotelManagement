459a2c9d018145b3bbbc6a0a5025900dcf32fdc8
Muyiam <liam.alejoasensi@gmail.com>
2024-05-02 17:06:04 +0200

    Adding the initial structure of the project

---
+++ HotelManagementFX/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module edu.carlosliam.hotelmanagementfx {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+
+    opens edu.carlosliam.hotelmanagementfx to javafx.fxml;
+    exports edu.carlosliam.hotelmanagementfx;
+    exports edu.carlosliam.hotelmanagementfx.controller;
+    opens edu.carlosliam.hotelmanagementfx.controller to javafx.fxml;
+}
