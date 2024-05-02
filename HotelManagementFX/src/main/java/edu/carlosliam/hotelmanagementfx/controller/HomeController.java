b94cc83ee8ba6bfa9318a7aea236e1a23207ecd4
Muyiam <liam.alejoasensi@gmail.com>
2024-05-02 23:39:13 +0200

    Added the Home page

---
+++ HotelManagementFX/src/main/java/edu/carlosliam/hotelmanagementfx/controller/HomeController.java
@@ -0,0 +1,50 @@
+package edu.carlosliam.hotelmanagementfx.controller;
+
+import edu.carlosliam.hotelmanagementfx.HotelManagementApplication;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+
+import java.io.IOException;
+
+public class HomeController {
+    @FXML
+    void goToAssignments() throws IOException {
+        changeScene("assignment-manager-view.fxml");
+    }
+
+    @FXML
+    void goToEmployees() throws IOException {
+        changeScene("employee-manager-view.fxml");
+    }
+
+    @FXML
+    void goToTasks() throws IOException {
+        changeScene("task-view.fxml");
+    }
+
+    @FXML
+    void openInfo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openSettings(ActionEvent event) {
+
+    }
+
+    @FXML
+    void close() {
+        Platform.exit();
+    }
+
+    private void changeScene(String sceneName) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                HotelManagementApplication.class.getResource(sceneName));
+        Scene scene = new Scene(fxmlLoader.load());
+
+        HotelManagementApplication.primaryStage.setScene(scene);
+    }
+}
