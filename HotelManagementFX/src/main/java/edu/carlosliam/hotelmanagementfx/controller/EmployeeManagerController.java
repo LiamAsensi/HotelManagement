78d6430bfd73b25289485a79205d3468a75fef27
Muyiam <liam.alejoasensi@gmail.com>
2024-05-02 23:40:22 +0200

    Added the toolbar to the Employee Manager

--- HotelManagementFX/src/main/java/edu/carlosliam/hotelmanagementfx/controller/EmployeeManagerController.java
+++ HotelManagementFX/src/main/java/edu/carlosliam/hotelmanagementfx/controller/EmployeeManagerController.java
@@ -1,4 +1,17 @@
 package edu.carlosliam.hotelmanagementfx.controller;
 
-public class EmployeeManagerController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class EmployeeManagerController implements Initializable {
+    @FXML
+    private HMToolBar toolbar;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        HMToolBar.disableButton(toolbar.btnGoToEmployees);
+    }
 }
