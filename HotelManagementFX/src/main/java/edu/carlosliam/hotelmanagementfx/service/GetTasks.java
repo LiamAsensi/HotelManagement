fd932c77a8db2d886e61639fc347df556fcd7293
carlosriverogv <karlos.riverog@gmail.com>
2024-05-12 13:30:29 +0200

    Solution to problem with LocalDate in GSON

--- HotelManagementFX/src/main/java/edu/carlosliam/hotelmanagementfx/service/GetTasks.java
+++ HotelManagementFX/src/main/java/edu/carlosliam/hotelmanagementfx/service/GetTasks.java
@@ -1,18 +1,21 @@
 package edu.carlosliam.hotelmanagementfx.service;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import edu.carlosliam.hotelmanagementfx.adapter.LocalDateAdapter;
 import edu.carlosliam.hotelmanagementfx.model.response.TaskListResponse;
 import edu.carlosliam.hotelmanagementfx.utils.ServiceUtils;
 import javafx.concurrent.Service;
 import javafx.concurrent.Task;
 
+import java.time.LocalDate;
+
 public class GetTasks extends Service<TaskListResponse> {
-    String filter;
+    private final String filter;
 
     public GetTasks()
     {
         filter="";
-
     }
     @Override
     protected Task<TaskListResponse> createTask() {
@@ -24,7 +27,9 @@ public class GetTasks extends Service<TaskListResponse> {
 
                 System.out.println(json);
 
-                Gson gson = new Gson();
+                Gson gson = new GsonBuilder()
+                        .registerTypeAdapter(LocalDate.class, new LocalDateAdapter())
+                        .create();
 
                 return gson.fromJson(json, TaskListResponse.class);
             }
