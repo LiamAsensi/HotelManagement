26782ab4290caeefbf2faf0683f4245a59175876
Muyiam <liam.alejoasensi@gmail.com>
2024-05-18 04:38:32 +0200

    Refactored Employee Manager, added options to generate reports

--- HotelManagementFX/src/main/java/edu/carlosliam/hotelmanagementfx/controller/EmployeeManagerController.java
+++ HotelManagementFX/src/main/java/edu/carlosliam/hotelmanagementfx/controller/EmployeeManagerController.java
@@ -6,19 +6,19 @@ import edu.carlosliam.hotelmanagementfx.service.DeleteEmployee;
 import edu.carlosliam.hotelmanagementfx.service.GetEmployees;
 import edu.carlosliam.hotelmanagementfx.model.data.Employee;
 import edu.carlosliam.hotelmanagementfx.service.GetEmployeesScheduled;
+import edu.carlosliam.hotelmanagementfx.utils.EmployeeReportPdfCreator;
 import edu.carlosliam.hotelmanagementfx.utils.MessageUtils;
 import edu.carlosliam.hotelmanagementfx.utils.ModalUtils;
-import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.concurrent.ScheduledService;
-import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ListView;
+import javafx.scene.layout.BorderPane;
 import javafx.util.Duration;
 
 import java.io.IOException;
@@ -28,24 +28,111 @@ import java.util.List;
 import java.util.ResourceBundle;
 
 public class EmployeeManagerController implements Initializable {
+    public static ObservableList<Employee> employeeObservableList;
+    public static GetEmployees getEmployees;
+    private static DeleteEmployee deleteEmployee;
+    private static GetEmployeesScheduled getEmployeesScheduled;
+    private static ProfessionFilter currentFilter = ProfessionFilter.ALL;
+
     @FXML
     private HMToolBar toolbar;
+    @FXML
+    private BorderPane root;
     @FXML
     private Button btnEditEmployee;
     @FXML
     private Button btnFire;
     @FXML
-    private Button btnPaycheck;
+    private ChoiceBox<String> cbProfession;
     @FXML
     private ListView<Employee> lvEmployees;
 
-    public static ObservableList<Employee> employeeObservableList;
-    public static GetEmployees getEmployees;
-    private static DeleteEmployee deleteEmployee;
-    private static GetEmployeesScheduled getEmployeesScheduled;
-
     public EmployeeManagerController() {
         employeeObservableList = FXCollections.observableArrayList();
+    }
+
+    // This method is used to filter the employees by profession
+    private static void filterEmployees(List<Employee> employees) {
+        if (currentFilter == ProfessionFilter.ALL) {
+            employeeObservableList.addAll(employees);
+        } else {
+            employees.stream()
+                    .filter(employee ->
+                            employee.getProfession().equals(
+                                    Employee.professionsEnum.get(currentFilter)
+                            )
+                    )
+                    .forEach(employeeObservableList::add);
+        }
+    }
+
+    /*
+    * This method is used to force the update of the items in the list view,
+    * without waiting for the scheduled service to update the list.
+    */
+    public static void forceUpdateOfItems() {
+        if (employeeObservableList == null) {
+            employeeObservableList = FXCollections.observableArrayList();
+        }
+        employeeObservableList.clear();
+        getEmployees = new GetEmployees();
+        getEmployees.start();
+
+        getEmployees.setOnSucceeded(e -> {
+            if (!getEmployees.getValue().isError()) {
+                employeeObservableList.clear();
+                filterEmployees(getEmployees.getValue().getResult());
+            } else {
+                MessageUtils.showError("Error getting tasks",
+                        getEmployees.getValue().getErrorMessage());
+            }
+        });
+
+        getEmployees.setOnFailed(e ->
+                MessageUtils.showError("Error", "Error connecting to the server"));
+    }
+
+    // This method is used to disable/enable the context buttons (edit and fire)
+    private void disableContextButtons(boolean disabled) {
+        btnEditEmployee.setDisable(disabled);
+        btnFire.setDisable(disabled);
+    }
+
+    // This method is used to load the professions checkbox and its event listener
+    private void loadProfessionsCheckbox() {
+        cbProfession.getItems().add("All");
+        cbProfession.getItems().addAll(Employee.professions.values());
+        cbProfession.getSelectionModel().selectFirst();
+
+        cbProfession.getSelectionModel()
+                .selectedItemProperty()
+                .addListener((observable, oldValue, newValue) -> {
+                            currentFilter = ProfessionFilter.valueOf(newValue.toUpperCase());
+                            forceUpdateOfItems();
+                        }
+                );
+    }
+
+    // This method is used to load the scheduled service
+    private void loadScheduledService() {
+        getEmployeesScheduled = new GetEmployeesScheduled();
+        getEmployeesScheduled.setPeriod(Duration.seconds(5));
+        getEmployeesScheduled.setOnSucceeded(e -> {
+            Employee selectedItem = lvEmployees.getSelectionModel().getSelectedItem();
+
+            if (getEmployeesScheduled.getValue() != null
+                    && !getEmployeesScheduled.getValue().isError()
+                    && getEmployeesScheduled.getValue() != employeeObservableList) {
+                employeeObservableList.clear();
+
+                filterEmployees(getEmployeesScheduled.getValue().getResult());
+
+                if (selectedItem != null && employeeObservableList.contains(selectedItem)) {
+                    lvEmployees.getSelectionModel().select(selectedItem);
+                }
+            }
+        });
+        getEmployeesScheduled.start();
     }
 
     @Override
@@ -53,17 +140,8 @@ public class EmployeeManagerController implements Initializable {
         HMToolBar.disableButton(toolbar.btnGoToEmployees);
         lvEmployees.setItems(employeeObservableList);
 
-        getEmployeesScheduled = new GetEmployeesScheduled();
-        getEmployeesScheduled.setPeriod(Duration.seconds(5));
-        getEmployeesScheduled.setOnSucceeded(e -> {
-            if (getEmployeesScheduled.getValue() != employeeObservableList && getEmployeesScheduled.getValue() != null) {
-                employeeObservableList.clear();
-                employeeObservableList.addAll(getEmployeesScheduled.getValue());
-            }
-        });
-        getEmployeesScheduled.start();
-
-        disableButtons(true);
+        disableContextButtons(true);
+        loadScheduledService();
 
         lvEmployees.setCellFactory(employeeListView -> {
             EmployeeListViewCell employeeListViewCell = new EmployeeListViewCell();
@@ -74,110 +152,42 @@ public class EmployeeManagerController implements Initializable {
         lvEmployees.getSelectionModel()
                 .selectedItemProperty()
                 .addListener(
-                        (observable, oldValue, newValue) -> disableButtons(false)
+                        (observable, oldValue, newValue) -> disableContextButtons(newValue == null)
                 );
 
-        btnFire.setOnAction(e -> fireEmployee());
+        root.setOnMouseClicked(e -> lvEmployees.getSelectionModel().clearSelection());
 
-        btnEditEmployee.setOnAction(e -> {
-            try {
-                editEmployee();
-            } catch (IOException ioException) {
-                ioException.printStackTrace();
-            }
-        });
-
-        btnPaycheck.setOnAction(e -> {
-            try {
-                generatePaycheck();
-            } catch (IOException ex) {
-                throw new RuntimeException(ex);
-            }
-        });
+        loadProfessionsCheckbox();
     }
 
     @FXML
     public void addEmployee() throws IOException {
         ModalUtils.openModal("layout/employee-new-view.fxml");
-        updateItems();
-        disableButtons(true);
+        forceUpdateOfItems();
+        lvEmployees.getSelectionModel().clearSelection();
     }
 
-    public static void updateItems() {
-        if (employeeObservableList == null) {
-            employeeObservableList = FXCollections.observableArrayList();
-        }
-        employeeObservableList.clear();
-        getEmployees = new GetEmployees();
-        getEmployees.start();
-
-        getEmployees.setOnSucceeded(e -> {
-            if (!getEmployees.getValue().isError()) {
-                employeeObservableList.addAll(getEmployees.getValue().getResult());
-            } else {
-                MessageUtils.showError("Error getting tasks", getEmployees.getValue().getErrorMessage());
-            }
-        });
-
-        getEmployees.setOnFailed(e -> {
-            MessageUtils.showError("Error", "Error connecting to the server");
-        });
-    }
-
-    private static ScheduledService<List<Employee>> createScheduledService() {
-        return new ScheduledService<>() {
-            @Override
-            protected Task<List<Employee>> createTask() {
-                return new Task<>() {
-                    @Override
-                    protected List<Employee> call() {
-                        List<Employee> employees = new ArrayList<>();
-                        System.out.println("Servicio creado...");
-
-                        getEmployees = new GetEmployees();
-                        getEmployees.start();
-
-                        getEmployees.setOnSucceeded(e -> Platform.runLater(() -> {
-                            if (!getEmployees.getValue().isError()) {
-                                System.out.println("Empleados obtenidos");
-                                employees.addAll(getEmployees.getValue().getResult());
-                            } else {
-                                MessageUtils.showError("Error getting tasks", getEmployees.getValue().getErrorMessage());
-                            }
-                        }));
-
-                        getEmployees.setOnFailed(e -> Platform.runLater(() -> {
-                            MessageUtils.showError("Error", "Error connecting to the server");
-                        }));
-
-                        System.out.println("Empleados obtenidos: " + employees.size());
-
-                        return employees;
-                    }
-                };
-            }
-        };
-    }
-
+    @FXML
     private void fireEmployee() {
         Employee employee = lvEmployees.getSelectionModel().getSelectedItem();
         if (employee != null) {
+            lvEmployees.getSelectionModel().clearSelection();
             deleteEmployee = new DeleteEmployee(employee.getId());
             deleteEmployee.start();
 
             deleteEmployee.setOnSucceeded(e -> {
-                System.out.println("a");
                 if (!deleteEmployee.getValue().isError()) {
-                    System.out.println("Socorrooo");
-                    updateItems();
-                    disableButtons(true);
+                    forceUpdateOfItems();
+                    disableContextButtons(true);
                 } else {
-                    MessageUtils.showError("Error firing employee", deleteEmployee.getValue().getErrorMessage());
+                    MessageUtils.showError("Error firing employee",
+                            deleteEmployee.getValue().getErrorMessage());
                 }
             });
         }
     }
 
+    @FXML
     private void editEmployee() throws IOException {
         Employee employee = lvEmployees.getSelectionModel().getSelectedItem();
         if (employee != null) {
@@ -188,17 +198,42 @@ public class EmployeeManagerController implements Initializable {
             controller.setEmployeeEdit(employee);
 
             ModalUtils.openModalParent(parent);
-            updateItems();
-            disableButtons(true);
+            forceUpdateOfItems();
+            lvEmployees.getSelectionModel().clearSelection();
         }
     }
 
+    @FXML
     private void generatePaycheck() throws IOException {
         ModalUtils.openModal("layout/employee-paycheck-view.fxml");
+        lvEmployees.getSelectionModel().clearSelection();
     }
 
-    private void disableButtons(boolean disabled) {
-        btnEditEmployee.setDisable(disabled);
-        btnFire.setDisable(disabled);
+    @FXML
+    private void generateEmployeeReport() {
+        EmployeeReportPdfCreator report = new EmployeeReportPdfCreator(
+                EmployeeReportPdfCreator.ReportType.ALL_EMPLOYEES,
+                new ArrayList<>(employeeObservableList)
+        );
+        report.createPdf();
+        MessageUtils.showMessage("Report created", "The report has been created successfully");
+
+        lvEmployees.getSelectionModel().clearSelection();
+    }
+
+    @FXML
+    private void generateExpenseReport() throws IOException {
+        ModalUtils.openModal("layout/employee-expense-report-view.fxml");
+        lvEmployees.getSelectionModel().clearSelection();
+    }
+
+    // This enum is used to store the state of the filter of the employees by profession
+    public enum ProfessionFilter {
+        ALL,
+        CLEANER,
+        MANAGER,
+        ELECTRICIAN,
+        PLUMBER,
+        CARPENTER
     }
 }
