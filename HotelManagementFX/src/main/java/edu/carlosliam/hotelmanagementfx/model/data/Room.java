af896a24ddabb6523773f46ad662db0d689f8373
Muyiam <liam.alejoasensi@gmail.com>
2024-05-30 21:45:21 +0200

    Changed the serialized name and added a getter

--- HotelManagementFX/src/main/java/edu/carlosliam/hotelmanagementfx/model/data/Room.java
+++ HotelManagementFX/src/main/java/edu/carlosliam/hotelmanagementfx/model/data/Room.java
@@ -3,10 +3,19 @@ package edu.carlosliam.hotelmanagementfx.model.data;
 import com.google.gson.annotations.SerializedName;
 
 public class Room {
-    @SerializedName("number")
+    @SerializedName("numero")
     private final int number;
 
     public Room(int number) {
         this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public String toString() {
+        return "Room number: " + number;
     }
 }
